af7ec08ecc81ba340189f640324708314f28287c
lukasz94w <lukasz94w@wp.pl>
2022-04-23 15:20:16 +0200

    Small fixes in method

--- src/Merge.java
+++ src/Merge.java
@@ -1,6 +1,10 @@
+import javax.print.attribute.standard.PresentationDirection;
+
 public class Merge {
 
     protected void method() {
         System.out.println("I'm some kind of method");
+
+        PresentationDirection presentationDirection;
     }
 }
