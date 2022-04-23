e503ac0ae85d2bb4b113f80f4f9cc28e847471d7
lukasz94w <lukasz94w@wp.pl>
2022-04-23 16:58:02 +0200

    Revert "Small fixes in method"
    This reverts commit af7ec08ecc81ba340189f640324708314f28287c.
    
    Revert commit

--- src/Merge.java
+++ src/Merge.java
@@ -1,10 +1,6 @@
-import javax.print.attribute.standard.PresentationDirection;
-
 public class Merge {
 
     protected void method() {
         System.out.println("I'm some kind of method");
-
-        PresentationDirection presentationDirection;
     }
 }
